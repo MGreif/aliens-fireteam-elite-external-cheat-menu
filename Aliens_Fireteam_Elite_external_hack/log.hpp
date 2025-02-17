b2715a42f120d5915b6b7af2634db86e4447bc04
Mika Greif <mika@greif.me>
2025-02-17 08:29:03 +0100

    Add: logging utils

---
+++ Aliens_Fireteam_Elite_external_hack/log.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#ifndef MG_LOG
+#define MG_LOG
+#include <stdio.h>
+
+#define RESET_COLOR 	"\x1B[0m"
+#define INFO_COLOR 	"\x1B[1;34m"
+#define DEBUG_COLOR 	"\x1B[1;30m"
+#define WARNING_COLOR 	"\x1B[1;33m"
+#define SUCCESS_COLOR 	"\x1B[1;32m"
+#define ERROR_COLOR 	"\x1B[31m"
+
+template <typename ...Args>
+static constexpr void log(const char* type, const char* color, const char* s, Args ...a) {
+    printf("%s[", color);
+    printf("%s]%s ", type, RESET_COLOR);
+    printf(s, a...);
+}
+template <typename ...Args> static constexpr void error_trace(const char* scope, const char* s, Args ...a) { 
+#ifdef _DEBUG_ERRORS
+    log("ERROR", ERROR_COLOR, s, a...); printf(" (%s) ", scope); if (GetLastError() > 0) log("ERROR", ERROR_COLOR, "%d\n", GetLastError());
+#endif // _DEBUG_ERRORS
+}
+template <typename ...Args> static constexpr void error(const char* s, Args ...a) { log("ERROR", ERROR_COLOR, s, a...); if (GetLastError() > 0) log("ERROR", ERROR_COLOR, "%d\n", GetLastError()); }
+template <typename ...Args> static constexpr void warning(const char* s, Args ...a) { log("WARNING", WARNING_COLOR, s, a...); }
+template <typename ...Args> static constexpr void info(const char* s, Args ...a) { log("INFO", INFO_COLOR, s, a...); }
+template <typename ...Args> static constexpr void success(const char* s, Args ...a) { log("SUCCESS", SUCCESS_COLOR, s, a...); }
+template <typename ...Args> static constexpr void debug(const char* s, Args ...a) { 
+#ifdef _DEBUG
+    log("DEBUG", DEBUG_COLOR, s, a...); 
+#endif
+}
+template <typename ...Args> static constexpr void exit(int status, const char* s, Args ...a) {
+    error(s, a...);
+    //getchar();
+    ExitProcess(status);
+}
+
+#endif // !MG_LOG
